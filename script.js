31c84fe287263ba2b5aecb694dd778340be2736a
gabriel <rdgfigueira.r6@gmail.com>
2024-01-11 22:12:40 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var campoCodigo = document.querySelector("#campoCodigo");
+var campoNome = document.querySelector("#campoNome");
+var botao = document.querySelector("#bntPesquisar");
+
+var pecas = {codidoDaPeca: 50291167, nomeDaPeca: "SOLEIRA EM LED RAMPAGE", corDaPeca: "Prata"} ;
+
+botao.addEventListener("click", function(e){
+    e.preventDefault();
+});
