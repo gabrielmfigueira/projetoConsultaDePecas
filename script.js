829e003e37f4b4893fc6f90a53042af63655e20a
gabriel <rdgfigueira.r6@gmail.com>
2024-01-14 10:27:24 -0300

    meu segundo commit

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ var campoCodigo = document.querySelector("#campoCodigo");
 var campoNome = document.querySelector("#campoNome");
 var botao = document.querySelector("#bntPesquisar");
 
-var pecas = {codidoDaPeca: 50291167, nomeDaPeca: "SOLEIRA EM LED RAMPAGE", corDaPeca: "Prata"} ;
+var pecas = [{codigoDaPeca: 50291167, nomeDaPeca: "SOLEIRA EM LED RAMPAGE", corDaPeca: "Prata"},{ codigoDaPeca: 52227286, nomeDaPeca: "DIVISOR DE CAÇAMBA", corDaPeca: "Preta"}] ;
 
 botao.addEventListener("click", function(e){
     e.preventDefault();
+    buscandoPecaPeloNome(campoNome);
 });
+
+function buscandoPecaPeloNome(nomePeca){
+
+var filtrandoPecaPeloNome =    
+
+    pecas.filter(function(obj)
+    {
+        return obj.nomeDaPeca == nomePeca.value;
+    });
+
+    console.log(filtrandoPecaPeloNome);
+}
