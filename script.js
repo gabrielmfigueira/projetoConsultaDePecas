5ff40575864b6ade8fb1f907e3d45e9f33477603
gabriel <rdgfigueira.r6@gmail.com>
2024-01-16 22:49:33 -0300

    meu quinto commit

--- script.js
+++ script.js
@@ -2,32 +2,34 @@ var campoCodigo = document.querySelector("#campoCodigo");
 var campoNome = document.querySelector("#campoNome");
 var botao = document.querySelector("#bntPesquisar");
 
-var campoTabelaNomeDaPeca = document.querySelector("#campoTabelaNomeDaPeca");
+var tabela = document.querySelector("#tabela");
 
 var pecas = [{codigoDaPeca: 50291167, nomeDaPeca: "SOLEIRA EM LED RAMPAGE", corDaPeca: "PRATA"},{ codigoDaPeca: 52227286, nomeDaPeca: "DIVISOR DE CAÇAMBA", corDaPeca: "PRETA"}] ;
 
 botao.addEventListener("click", function(e){
     e.preventDefault();
+    tabela.innerHTML = ``;
     buscandoPecaPeloNome(campoNome.value);
 });
 
 function buscandoPecaPeloNome(nomePeca){
 
-    pecas.filter(function()
-    {
+    for( var i = 0; i < pecas.length; i++){
 
-        for( var i = 0; i < pecas.length; i++){
+        if(pecas[i].nomeDaPeca.includes(nomePeca.toLocaleUpperCase()) == true){
+            tabela.innerHTML += 
+            `
+                <tr>
+                    <td>${pecas[i].codigoDaPeca}</td>
+                    <td>${pecas[i].nomeDaPeca}</td>
+                    <td>${pecas[i].corDaPeca}</td>
+                </tr>
+            `
 
-            if(pecas[i].nomeDaPeca.includes(nomePeca.toLocaleUpperCase()) == true){
-                campoTabelaNomeDaPeca.innerHTML += pecas[i].nomeDaPeca;
-            }else{
-
-            }
+        }else{
 
         }
-    });
-
-    
+    }
 }
 
 // if(pecas[1].nomeDaPeca.includes(campoNome.value) == true){console.log(10)};
