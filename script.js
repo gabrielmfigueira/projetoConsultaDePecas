e40578202f83a816f5dddc4bed4de8af7d0aaf7f
gabriel <rdgfigueira.r6@gmail.com>
2024-01-28 22:54:29 -0300

    meu décimo terceiro commit

--- script.js
+++ script.js
@@ -3,34 +3,39 @@ var campoNome = document.querySelector("#campoNome");
 var botao = document.querySelector("#bntPesquisar");
 
 var tabela = document.querySelector("#tabela");
+   
+const jsonAcessorios = "acessorios.json"; 
 
-var pecas = [{codigoDaPeca: 50291167, nomeDaPeca: "SOLEIRA EM LED RAMPAGE", corDaPeca: "PRATA"},
-             {codigoDaPeca: 52227286, nomeDaPeca: "DIVISOR DE CAÇAMBA", corDaPeca: "PRETA"}
-            ];
+botao.addEventListener("click", function(e){ 
 
-botao.addEventListener("click", function(e){
     e.preventDefault();
     tabela.innerHTML = ``;
     buscandoPecaPeloNome(campoNome.value);
 });
 
 function buscandoPecaPeloNome(nomePeca){
+  
+    fetch(jsonAcessorios)
+    .then(res => res.json())
+    .then(dados => {
 
-    for( var i = 0; i < pecas.length; i++){
+        for( var i = 0; i < dados.acessorios.length; i++){
 
-        if(pecas[i].nomeDaPeca.includes(nomePeca.toLocaleUpperCase()) == true){
-            tabela.innerHTML += 
-            `
-                <tr>
-                    <td>${pecas[i].codigoDaPeca}</td>
-                    <td>${pecas[i].nomeDaPeca}</td>
-                    <td>${pecas[i].corDaPeca}</td>
-                </tr>
-            `
-        }else{
-
+            if(dados.acessorios[i].nomeDaPeca.includes(nomePeca.toLocaleUpperCase()) == true){
+                tabela.innerHTML += 
+                `
+                    <tr>
+                        <td>${dados.acessorios[i].codigoDaPeca}</td>
+                        <td>${dados.acessorios[i].nomeDaPeca}</td>
+                        <td>${dados.acessorios[i].corDaPeca}</td>
+                    </tr>
+                `
+            }else{
+    
+            }
         }
-    }
+    });  
+
 }
 
 function buscandoPecaPeloCodigo(nomePeca){
